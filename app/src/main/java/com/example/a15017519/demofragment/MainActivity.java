582594d1e1f818e9c3c076040a3f7486cc2eee30
15017519 <15017519@myrp.edu.sg>
2017-06-02 11:05:12 +0800

    done

--- app/src/main/java/com/example/a15017519/demofragment/MainActivity.java
+++ app/src/main/java/com/example/a15017519/demofragment/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.a15017519.demofragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import layout.FragmentFirst;
+import layout.FragmentSecond;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment f1 = new FragmentFirst();
+        ft.replace(R.id.frame1, f1);
+
+        Fragment f2 = new FragmentSecond();
+        ft.replace(R.id.frame2, f2);
+
+        ft.commit();
     }
 }
+
